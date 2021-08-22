3e66f7b9cf5d84d16a43aa94d01a72b6abd5c21d
Joel N. Walkley <joelnwalkley@gmail.com>
2021-08-22 10:47:36 -0400

    add Southern and Eastern regions for export

--- globals.js
+++ globals.js
@@ -84,6 +84,86 @@ var EasternColor = "#2159bc";
 var allFips = ["51510","51515","51520","51530","51540","51550","51570","51580","51590","51595","51600","51610","51620","51630","51640","51650","51660","51670","51678","51680","51683","51685","51690","51700","51710","51720","51730","51735","51740","51750","51760","51770","51775","51790","51800","51810","51820","51830","51840","51001","51003","51005","51007","51009","51011","51013","51015","51017","51019","51021","51023","51025","51027","51029","51031","51033","51035","51036","51037","51041","51043","51045","51047","51049","51051","51053","51057","51059","51061","51063","51065","51067","51069","51071","51073","51075","51077","51079","51081","51083","51085","51087","51089","51091","51093","51095","51097","51099","51101","51103","51105","51107","51109","51111","51113","51115","51117","51119","51121","51125","51127","51131","51133","51135","51137","51139","51141","51143","51145","51147","51149","51153","51155","51157","51159","51161","51163","51165","51167","51169","51171","51173","51175","51177","51179","51181","51183","51185","51187","51191","51193","51195","51197","51199"];
 
 //Define CSUs
+//Eastern Region
+const CSU1 = {
+    localities: ["51550"],
+    name: "District 1 - Chesapeake",
+    slug: "CSU1"
+};
+const CSU2 = {
+    localities: ["51810"],
+    name: "District 2 - Virginia Beach",
+    slug: "CSU2"
+};
+const CSU2A = {
+    localities: ["51001","51131"],
+    name: "District 2A - Accomack",
+    slug: "CSU2A"
+};
+const CSU3 = {
+    localities: ["51740"],
+    name: "District 3 - Portsmouth",
+    slug: "CSU3"
+};
+const CSU4 = {
+    localities: ["51710"],
+    name: "District 4 - Norfolk",
+    slug: "CSU4"
+};
+const CSU5 = {
+    localities: ["51800","51620","51093","51175"],
+    name: "District 5 - Suffolk",
+    slug: "CSU5"
+};
+const CSU7 = {
+    localities: ["51700"],
+    name: "District 7 - Newport News",
+    slug: "CSU7"
+};
+const CSU8 = {
+    localities: ["51650"],
+    name: "District 8 - Hampton",
+    slug: "CSU8"
+};
+const EasternRegion = {
+    CSUs: [CSU1,CSU2,CSU2A,CSU3,CSU4,CSU5,CSU7,CSU8],
+    name: "Eastern Region"
+}
+//Southern Region
+const CSU6 = {
+    localities: ["51670","51149","51181","51183","51595","51025"],
+    name: "District 6 - Hopewell",
+    slug: "CSU6"
+};
+const CSU10 = {
+    localities: ["51011","51029","51037","51049","51083","51111","51117","51147"],
+    name: "District 10 - Halifax",
+    slug: "CSU10"
+};
+const CSU11 = {
+    localities: ["51730","51007","51053","51135","51145"],
+    name: "District 11 - Petersburg",
+    slug: "CSU11"
+};
+const CSU12 = {
+    localities: ["51041","51570"],
+    name: "District 12 - Chesterfield",
+    slug: "CSU12"
+};
+const CSU13 = {
+    localities: ["51760"],
+    name: "District 13 - Richmond",
+    slug: "CSU13"
+};
+const CSU14 = {
+    localities: ["51087"],
+    name: "District 14 - Henrico",
+    slug: "CSU14"
+};
+const SouthernRegion = {
+    CSUs: [CSU6,CSU10,CSU11,CSU12,CSU13,CSU14],
+    name: "Southern Region"
+}
 //Northern Region
 var CSU17 = {
     localities: ["51013","51610"],
@@ -193,6 +273,7 @@ var CentralRegion = {
 //put CSUs all together
 var CSUStructure = [NorthernRegion,CentralRegion,WesternRegion];
 var sortedCSUs = [CSU9,CSU15,CSU16,CSU17,CSU18,CSU19,CSU20,CSU21,CSU22,CSU23,CSU24,CSU25,CSU26,CSU27,CSU28,CSU29,CSU30,CSU31];
+const sortedCSUsALL = [CSU1,CSU2,CSU2A,CSU3,CSU4,CSU5,CSU6,CSU7,CSU8,CSU9,CSU10,CSU11,CSU12,CSU13,CSU14,CSU15,CSU16,CSU17,CSU18,CSU19,CSU20,CSU21,CSU22,CSU23,CSU24,CSU25,CSU26,CSU27,CSU28,CSU29,CSU30,CSU31];
 
 //for use in defining localities
 var localityUrl = "locality.php?";
