166de8f41a104e2241e7f4adecd1de60e9501f12
Joel N. Walkley <joelnwalkley@gmail.com>
2024-05-09 11:24:44 -0400

    DSP-10 add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // The entry point file described above
+  entry: "./index.js",
+  // The location of the build folder described above
+  output: {
+    path: __dirname,
+    filename: "bundle.js",
+  },
+};
