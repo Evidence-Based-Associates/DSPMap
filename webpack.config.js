a9c90a4825d71d86eace3570e74e486c712e717a
Joel N. Walkey <joelnwalkley@gmail.com>
2024-05-21 20:47:05 -0400

    DSP-8 without html plugin need static src import

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,55 +6,12 @@ module.exports = {
     "pages/somePage/index": "./src/pages/somePage/index.js",
     "pages/anotherPage/index": "./src/pages/anotherPage/index.js",
   },
-  // entry: ["./src/pages/somePage/index.js", "./src/pages/anotherPage/index.js"],
   devtool: "inline-source-map",
   devServer: {
-    static: "./dist",
+    static: "./docs",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/pages/somePage/index.html",
-      filename: "pages/somePage/index.html",
-      chunks: ["pages/somePage/index"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "src/pages/anotherPage/index.html",
-      filename: "pages/anotherPage/index.html",
-      chunks: ["pages/anotherPage/index"],
-    }),
-  ],
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
-      },
-    ],
+    path: path.resolve(__dirname, "docs"),
   },
 };
