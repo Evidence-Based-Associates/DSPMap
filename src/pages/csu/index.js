38c16a2d00241cda449aa749a6c2de2ca8e9cbdf
Joel N. Walkey <joelnwalkley@gmail.com>
2024-05-25 15:38:31 -0400

    DSP-13 working csu service and provider list

--- src/pages/csu/index.js
+++ src/pages/csu/index.js
@@ -7,42 +7,36 @@ import {
 } from "./api.js";
 
 const csuNameSpan = document.getElementById("csuName");
-csuNameSpan.innerHTML = thisCSU.name;
+if (csuNameSpan) {
+  csuNameSpan.innerHTML = thisCSU?.name || "CSU";
+}
 
-// TODO: Investigate styling for ebablue list items
 const providerList = document.getElementById("providerList");
 CSUProviders.forEach(async (provider, key) => {
-  const providerLI = document.createElement("li");
-  providerLI.classList.add("ebaBlue");
-  providerLI.innerHTML = `<a href='../provider/index.html?id=${key}'>${provider}</a>`;
-  providerList.appendChild(providerLI);
   const providerServices = await providerServiceList(key, thisCSU);
-
-  const providerServicesList = document.createElement("ul");
-  providerServices.forEach((service) => {
-    const serviceLI = document.createElement("li");
-    serviceLI.innerText = service;
-    providerServicesList.appendChild(serviceLI);
-  });
-  providerList.appendChild(providerServicesList);
+  const list = headingList(provider, providerServices);
+  providerList?.appendChild(list);
 });
 
 const serviceList = document.getElementById("serviceList");
 CSUServices.forEach(async (service) => {
-  const serviceLI = document.createElement("li");
-  serviceLI.classList.add("ebaBlue");
-  serviceLI.innerText = service;
-  serviceLI.id = `${service}-provider-list`;
-
-  serviceList.appendChild(serviceLI);
   const serviceProviders = await serviceProvidersList(service, thisCSU);
-
-  const serviceProvidersUL = document.createElement("ul");
-  serviceProvidersUL.id = `${service}-providers`;
-  serviceProviders.forEach((provider, key) => {
-    const providerLI = document.createElement("li");
-    providerLI.innerHTML = `<a href='../provider/index.html?id=${key}'>${provider}</a>`;
-    serviceProvidersUL.appendChild(providerLI);
-  });
-  serviceLI.appendChild(serviceProvidersUL);
+  const list = headingList(service, serviceProviders);
+  serviceList?.appendChild(list);
 });
+
+const headingList = (headingText, listItems) => {
+  const container = document.createElement("div");
+  const heading = document.createElement("h3");
+  heading.classList.add("ebaBlue");
+  heading.innerText = headingText;
+  const list = document.createElement("ul");
+  listItems.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerText = item;
+    list.appendChild(li);
+  });
+  container.appendChild(heading);
+  container.appendChild(list);
+  return container;
+};
