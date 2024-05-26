297f43b5ab06aaa9923c6139b5fb618961406fe5
Joel N. Walkey <joelnwalkley@gmail.com>
2024-05-26 07:06:28 -0400

    DSP-13 with form elements

--- src/pages/admin/provider/api.js
+++ src/pages/admin/provider/api.js
@@ -1,1 +1,8 @@
-export const testMsg = "hello world";
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const updateProviderName = urlParams.get("name");
+
+export const isNew = updateProviderName === null;
+export const headerText = isNew
+  ? "Add New Provider"
+  : `Updating ${updateProviderName}`;
--- src/pages/admin/provider/index.js
+++ src/pages/admin/provider/index.js
@@ -1,3 +1,21 @@
-import { testMsg } from "./api";
+import { isNew, headerText } from "./api";
 
-console.log(testMsg);
+const headerTextSpan = document.getElementById("headerText");
+if (headerTextSpan) {
+  headerTextSpan.innerHTML = headerText;
+}
+
+const providerForm = document.getElementById("providerForm");
+const showData = () => {
+  // @ts-ignore
+  const formData = new FormData(providerForm);
+
+  for (const [key, value] of formData) {
+    console.log(`${key}: ${value}`);
+  }
+};
+
+const submitButton = document.getElementById("submitButton");
+if (submitButton) {
+  submitButton.addEventListener("click", showData);
+}
