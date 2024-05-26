1f543d37797990303c001e935e4ae25d122f6771
Joel N. Walkey <joelnwalkley@gmail.com>
2024-05-26 07:14:42 -0400

    DSP-14 with defaultMapZoom select

--- src/pages/admin/provider/index.js
+++ src/pages/admin/provider/index.js
@@ -1,8 +1,19 @@
 import { isNew, headerText } from "./api";
+import { CSUStructure } from "../../../lib/csu";
 
 const headerTextSpan = document.getElementById("headerText");
 if (headerTextSpan) {
   headerTextSpan.innerHTML = headerText;
+}
+
+const defaultMapZoomSelect = document.getElementById("defaultMapZoom");
+if (defaultMapZoomSelect) {
+  CSUStructure.forEach((region, index) => {
+    const option = document.createElement("option");
+    option.value = index.toString();
+    option.text = region.name;
+    defaultMapZoomSelect.appendChild(option);
+  });
 }
 
 const providerForm = document.getElementById("providerForm");
