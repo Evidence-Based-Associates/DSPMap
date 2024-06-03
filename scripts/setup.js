4952f31848b3be0ca5b2bda2b21fad9a2b44610f
Joel N. Walkey <joelnwalkley@gmail.com>
2024-06-03 06:23:56 -0400

    firebase restore devConfig

--- scripts/setup.js
+++ scripts/setup.js
@@ -5,16 +5,15 @@ const config = {
   ENV: "LOCAL",
 };
 
-const devConfig = {};
-// {
-//   apiKey: "AIzaSyC6X571rXek2TC8XCE8jcd6bIgKi5sc0_A",
-//   authDomain: "eba-dsp-map-dev.firebaseapp.com",
-//   projectId: "eba-dsp-map-dev",
-//   storageBucket: "eba-dsp-map-dev.appspot.com",
-//   messagingSenderId: "200138947447",
-//   appId: "1:200138947447:web:c7b66df19e77ec8f237f72",
-//   measurementId: "G-SNEMBJE4DT",
-// };
+const devConfig = {
+  apiKey: "AIzaSyC6X571rXek2TC8XCE8jcd6bIgKi5sc0_A",
+  authDomain: "eba-dsp-map-dev.firebaseapp.com",
+  projectId: "eba-dsp-map-dev",
+  storageBucket: "eba-dsp-map-dev.appspot.com",
+  messagingSenderId: "200138947447",
+  appId: "1:200138947447:web:c7b66df19e77ec8f237f72",
+  measurementId: "G-SNEMBJE4DT",
+};
 
 const emulatorConfig = {
   projectId: "demo-dsp-map",
