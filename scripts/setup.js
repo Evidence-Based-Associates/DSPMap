8b5cd830a7900fd9cd39c67592d88c8948d0b537
Joel N. Walkey <joelnwalkley@gmail.com>
2024-06-27 11:24:59 -0400

    DSP-18 modify import/export for multiple envs

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
-const config = {
-  ENV: "LOCAL",
-};
+// read from process
+// options include DEV, PROD, LOCAL
+// example: ENV=PROD node import.js
+const ENV = process.env.ENV || "DEV";
 
 const devConfig = {
   apiKey: "AIzaSyC6X571rXek2TC8XCE8jcd6bIgKi5sc0_A",
@@ -15,12 +16,28 @@ const devConfig = {
   measurementId: "G-SNEMBJE4DT",
 };
 
+const prodConfig = {
+  apiKey: "AIzaSyC6Lg3lKbT9mSHavFTxIGpub9hiyWGjBPQ",
+  authDomain: "eba-dsp-map-prod.firebaseapp.com",
+  projectId: "eba-dsp-map-prod",
+  storageBucket: "eba-dsp-map-prod.appspot.com",
+  messagingSenderId: "1068377714544",
+  appId: "1:1068377714544:web:5999d487c7ef4aca3c69b5",
+  measurementId: "G-RDPWS5CTYM",
+};
+
 const emulatorConfig = {
   projectId: "demo-dsp-map",
 };
 
-const isLocal = config.ENV === "LOCAL";
-const firebaseConfig = isLocal ? emulatorConfig : devConfig;
+const isLocal = ENV === "LOCAL";
+const envFirebaseConfig = {
+  LOCAL: emulatorConfig,
+  DEV: devConfig,
+  PROD: prodConfig,
+};
+
+const firebaseConfig = envFirebaseConfig[ENV];
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
